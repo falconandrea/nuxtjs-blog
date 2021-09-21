a2c6f7c8dd09693a6016d800ba0a3ea2779c2d9d
Andrea Falcon <923390-andrea.falcon@users.noreply.gitlab.com>
2021-09-21 16:27:27 +0200

    feat: create Sidebar and update index

---
+++ components/Sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="p-4 w-full lg:w-1/3">
+    <h3 class="mt-4 mb-2">Cerca tra i post</h3>
+    <form method="GET" action="" class="flex flex-row">
+      <input id="search-input" class="border-2 rounded-xl px-4 py-2" type="text" name="search" placeholder="Laravel, Vue ..."/>
+      <button class="px-4 text-white bg-blue-300 rounded-xl ml-2" type="submit">
+          Cerca
+      </button>
+    </form>
+
+    <h3 class="mt-4 mb-2">Tags</h3>
+    <ListTags :tags=tags></ListTags>
+  </div>
+</template>
+
+<script>
+import ListTags from '../components/ListTags.vue'
+export default {
+  components: {
+    ListTags
+  },
+  data () {
+    return {
+      tags: []
+    }
+  },
+  created() {
+    // Get tags
+    const tags = [
+      {
+        'name': 'Laravel', 'slug': 'laravel',
+      },
+      {
+        'name': 'PHP', 'slug': 'php',
+      },
+      {
+        'name': 'Vue', 'slug': 'vue',
+      }
+    ]
+    this.tags = tags
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <Header></Header>
+    <div class="container flex flex-col lg:flex-row mx-auto pb-4">
+      <ListPosts :posts="posts"></ListPosts>
+      <Sidebar />
+    </div>
     <Footer></Footer>
   </div>
 </template>
@@ -8,9 +12,31 @@
 <script>
 import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
+import ListPosts from '../components/ListPosts.vue'
+import Sidebar from '../components/Sidebar.vue'
 export default {
   components: {
-    Header, Footer
+    Header, Footer, ListPosts, Sidebar
+  },
+  data() {
+    return {
+      posts: {
+        type: Array,
+        default: null
+      }
+    }
+  },
+  created() {
+    // Get tags
+    const posts = [
+      {
+        'title': 'Primo post di prova', 'intro': 'Breve descrizione del post', 'date': '21/09/2021', 'slug': 'primo-post-di-prova', 'tags': [{'name': 'Laravel', 'slug': 'laravel'}, {'name': 'PHP', 'slug': 'php'}]
+      },
+      {
+        'title': 'Secondo post di prova', 'intro': 'Breve descrizione del post nuovo', 'date': '20/09/2021', 'slug': 'secondo-post-di-prova', 'tags': [{'name': 'Laravel', 'slug': 'laravel'}, {'name': 'PHP', 'slug': 'php'}, {'name': 'Vue', 'slug': 'vue'}]
+      }
+    ]
+    this.posts = posts
   }
 }
 </script>
