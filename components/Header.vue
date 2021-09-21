4c79bea61595b7d33317b7182615781d0b3c100a
Andrea Falcon <923390-andrea.falcon@users.noreply.gitlab.com>
2021-09-21 14:39:21 +0200

    feat: update header and footer

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="bg-blue-300">
+    <div class="container mx-auto px-4">
+      <h2 class="py-4">My personal blog</h2>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
