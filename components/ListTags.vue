ee09e2bce9b5ace0478cef09801b00aef18a0ec9
Andrea Falcon <923390-andrea.falcon@users.noreply.gitlab.com>
2021-09-21 16:26:26 +0200

    feat: create Tag and ListTags components

---
+++ components/ListTags.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-row">
+    <template v-for="tag in tags">
+      <Tag :key="tag.slug" :name="tag.name" :slug="tag.slug"></Tag>
+    </template>
+  </div>
+</template>
+
+<script>
+import Tag from '../components/Tag.vue'
+export default {
+  components: {
+    Tag,
+  },
+  props: {
+    tags: {
+      type: Array,
+      default: null
+    }
+  }
+}
+</script>
---
+++ components/Tag.vue
@@ -0,0 +1,25 @@
+<template>
+  <a :href="url" class="text-sm text-gray-500 px-2 py-1 rounded-xl bg-blue-100 hover:bg-blue-400 hover:text-white mr-2" :title="name">
+    {{ name }}
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: null
+    },
+    slug: {
+      type: String,
+      default: null
+    }
+  },
+  data() {
+    return {
+      url: `tags/${ this.slug }`
+    }
+  }
+}
+</script>
