01670081c7f26e062643d01e0a1a12ffe5cb069e
Andrea Falcon <923390-andrea.falcon@users.noreply.gitlab.com>
2021-11-05 19:59:10 +0100

    fix: prismjs and add temporary custom css

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,11 +17,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: ['~/plugins/prism.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/prism'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
