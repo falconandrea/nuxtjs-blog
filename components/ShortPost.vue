5c27d8db3d0a00785bff8d4d309b3173ca3b5f6d
Andrea Falcon <923390-andrea.falcon@users.noreply.gitlab.com>
2021-11-09 18:22:21 +0100

    feat: use slug in post url

--- components/ShortPost.vue
+++ components/ShortPost.vue
@@ -2,7 +2,7 @@
   <div class="w-full border-b-2 py-4">
     <p><small>{{ post.date }}</small></p>
     <h4 class="font-medium text-2xl py-1">
-      <router-link :to="`/post/${post.id}`" title="" class="text-gray-700 hover:text-blue-400">{{ post.title }}</router-link>
+      <router-link :to="`/post/${post.slug}`" title="" class="text-gray-700 hover:text-blue-400">{{ post.title }}</router-link>
     </h4>
     <ListTags :tags="post.tags"></ListTags>
     <p class="pt-2 text-gray-500">{{ post.intro }}</p>
