6847f002db3e91dac91e587b6546dd6b2882c929
Andrea Falcon <andrea.falcon@heply.it>
2021-11-15 19:43:50 +0100

    chore: add axios-debug-log for debug

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 export default function ({ $axios, redirect }) {
   $axios.baseURL = process.env.API_URL
-
   $axios.onRequest(config => {
+    require('axios-debug-log')
     config.headers.common['Content-Type'] = 'application/json'
     $axios.defaults.xsrfHeaderName = 'X-CSRF-TOKEN'
   })
